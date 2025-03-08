5941b2d97141e1b5f5c682182c8e62d303410b80
sethu200684 <sethumdee.20231384@iit.ac.lk>
2025-03-08 14:51:06 +0530

    Created content of news page

---
+++ src/js/news.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const applyButtons = document.querySelectorAll(".view-project-btn");
+
+    
+});
+
+function toggleDropdown() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
+
