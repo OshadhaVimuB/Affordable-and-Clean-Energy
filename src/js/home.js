bf7cb8001e8040d96ee9104e54569b98b6c66332
Oshadha Vimukthi <oshadhavjb@gmail.com>
2025-03-09 15:21:16 +0530

    Finalized the Table page and fixed errors in the site

--- src/js/home.js
+++ src/js/home.js
@@ -7,3 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     hamburger.classList.toggle("active");
   });
 });
+
+function visitLink() {
+  window.location.href = "/pages/table.html";
+}
